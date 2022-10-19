da8c2bb7206d80c0a2dd7e1382329071f4d9e353
tenshi-cs <116143418+tenshi-cs@users.noreply.github.com>
2022-10-19 11:34:09 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = "10247139"
+API_HASH = "96b46175824223a33737657ab943fd6a"
+BOT_TOKEN = "5609860345:AAE3XIuiOKxSR5Js3W6vRO2-0t6JTldaXhg"
+
+
+
+MONGO_DB_URI = "mongodb+srv://1080pv2:1234@cluster0.kkmuftd.mongodb.net/?retryWrites=true&w=majority"
+INDEX_ID = -1001612459325
+UPLOADS_ID = -1001863513396
+PROGRESS_ID = -1001863513396
+LINK_ID = -1001822793615
+
+STATUS_ID = 9
+SCHEDULE_ID = 3
+
+CHANNEL_TITLE = "Airing animes hardsub"
+INDEX_USERNAME = "animext720p_posters"
+UPLOADS_USERNAME = "animext720p_posters"
